14fc3154b5cb37b181342634a7cbdf0da3cc5100
DharmendraVaghela <dharmendra89g@gmail.com>
2016-11-29 19:17:16 -0500

    Updates for steming and new call

--- data_analysis.py
+++ data_analysis.py
@@ -1,35 +1,40 @@
 import json
 import tempfile
 import pandas
-from stemming.porter2 import stem
 import os
 import os.path
+import textwrap
+from nltk import PorterStemmer
+from stemming.porter2 import stem
 
 from nltk.corpus import stopwords
 
-from watson_developer_cloud import AlchemyLanguageV1 as Alchemy
+from watson_developer_cloud import AlchemyLanguageV1 as Alc
 
-class ALC(object): #wrapper
+cachedStopWords = stopwords.words("english")
 
-    def __init__(self, url, note, apikey, id, corpus):
-        self.alc = Alchemy(url=url, note=note, apikey=apikey)
+def stopwords_fn(text):
+    #text = 'hello bye the the hi'
+    return  (' '.join([word for word in text.split() if word not in stopwords.words("english")]))
+
+def excall(url,api_key,text):
+    alc = Alc(url=url, api_key=api_key)
+    return (json.dumps(alc.keywords(text=text, sentiment=True)))
+
+class ALCH(object): #wrapper
+
+    def __init__(self, text, apikey, id):
+        self.alc = Alc(api_key=apikey)
+        self.text = text
         self.id = id
-        self.corpus = corpus
 
-    def __repr__(self):
-        return "Alchemy Results: %s" % self.id
 
     def author(self, html=None, url=None, language=None):
         params = {'language': language}
         return self._alchemy_html_request('GetAuthor', html=html, url=url, params=params)
 
-    def authors(self, html=None, url=None, language=None):
-        params = {'language': language}
-        return self._alchemy_html_request('GetAuthors', html=html, url=url, params=params)
-
     def keywords(self, html=None, text=None, url=None, strict_extract_mode=False, sentiment=False, emotion=False,
                  show_source_text=False, max_items=None, language=None, max_keywords=50):
-
         if not max_items:
             max_items = max_keywords
         params = {'keywordExtractMode': 'strict' if strict_extract_mode else 'normal',
@@ -38,8 +43,8 @@ class ALC(object): #wrapper
                   'showSourceText': show_source_text,
                   'maxRetrieve': max_items,
                   'language': language}
-        return self._alchemy_html_request('GetRankedKeywords', html=html, text=text, url=url, params=params)
-
+        #return self._alchemy_html_request('GetRankedKeywords', html=html, text=text, url=url, params=params)
+        return self.alc.keywords(text=self.text, sentiment=True)
     def concepts(self, html=None, text=None, url=None, max_items=8, linked_data=True, show_source_text=False,
                  language=None):
         params = {'maxRetrieve': max_items,
@@ -48,16 +53,52 @@ class ALC(object): #wrapper
                   'language': language}
         return self._alchemy_html_request('GetRankedConcepts', html=html, text=text, url=url, params=params)
 
-    '''{
+
+    def entities(self, html=None, text=None, url=None, disambiguate=True, linked_data=True, coreference=True,
+                 quotations=False, sentiment=False, emotion=False, show_source_text=False, max_items=50, language=None,
+                 model=None):
+        params = {'disambiguate': disambiguate,
+                  'linkedData': linked_data,
+                  'coreference': coreference,
+                  'quotations': quotations,
+                  'sentiment': sentiment,
+                  'emotion': emotion,
+                  'showSourceText': show_source_text,
+                  'maxRetrieve': max_items,
+                  'language': language,
+                  'model': model}
+        return self._alchemy_html_request('GetRankedNamedEntities', html=html, text=text, url=url, params=params)
+
+    def emotion(self, html=None, text=None, url=None, show_source_text=False, source_text_type=None,
+                constraint_query=None, xpath_query=None, language=None):
+        params = {'showSourceText': show_source_text,
+                  'sourceText': source_text_type,
+                  'cquery': constraint_query,
+                  'xpath': xpath_query,
+                  'language': language}
+        return self._alchemy_html_request('GetEmotion', html=html, text=text, url=url, params=params)
+
+    def category(self, html=None, text=None, url=None, show_source_text=False, language=None):
+        params = {'showSourceText': show_source_text, 'language': language}
+        return self._alchemy_html_request('GetCategory', html=html, text=text, url=url, params=params)
+
+
+
+
+'''{
       "url": "https://gateway-a.watsonplatform.net/calls",
       "note": "It may take up to 5 minutes for this key to become active",
       "apikey": "32faaeff82652a00ba0e804f5c84e221caa9e1af"
     }'''
 
-default_url = 'https://gateway-a.watsonplatform.net/calls'
-apikey1 = '32faaeff82652a00ba0e804f5c84e221caa9e1af'
-note = ''
 
+default_url = 'https://gateway-a.watsonplatform.net/calls'
+#apikey1 = '32faaeff82652a00ba0e804f5c84e221caa9e1af'
+apikey1 = 'ec101d759932b007888d0dae6d3882a0204aa1d8'
+
+
+
+note = ''
 path = '' #current dir
 #df = pandas.read_csv(os.path.join(path, "input.csv".format(str(x))))
 
@@ -77,10 +118,34 @@ example = 'dont like alcohol dont take marijuana dont attend parties with loads 
               'dont like alcohol dont take marijuana dont attend parties with loads of alcohol, beer and wine'
 
 
-obj = ALC(default_url,note = '',apikey=apikey1,id = '', corpus= '')
-obj.keywords(text=example)
-obj.author()
+#inst1 = ALC(example,apikey1,1)
+#inst1.author()
+#inst1.keywords()
 
-obj.authors()
-alc = Alchemy(url=default_url, api_key=apikey1)
-print(json.dumps(alc.keywords(text = example, sentiment= True)))
+
+splited = example.split()
+new = []
+
+for words in splited:
+    new.append(stem(words))
+
+
+stemmed = PorterStemmer().stem_word(example)
+
+stemmed2 = stem(example)
+
+wostopw = stopwords_fn(example)
+
+
+
+file = excall(default_url,apikey1,example)
+print file
+
+
+
+#alc = Alc(url=default_url, api_key=apikey1)
+#print(json.dumps(alc.keywords(text = example, sentiment= True)))
+
+
+
+
